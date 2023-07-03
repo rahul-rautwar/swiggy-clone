939e1e297a47efebdc0dfbda45caa6c98add52b7
rahul-rautwar <rahulrautwar@gmail.com>
2023-07-03 22:12:26 +0530

    add more restro card

--- src/components/Body.js
+++ src/components/Body.js
@@ -7,6 +7,24 @@ const Body = () => {
       <div className="search">Search</div>
       <div className="res-container">
         <RestaurantCard></RestaurantCard>
+        <RestaurantCard></RestaurantCard>
+        <RestaurantCard></RestaurantCard>
+        <RestaurantCard></RestaurantCard>
+        <RestaurantCard></RestaurantCard>
+        <RestaurantCard></RestaurantCard>
+        <RestaurantCard></RestaurantCard>
+        <RestaurantCard></RestaurantCard>
+        <RestaurantCard></RestaurantCard>
+        <RestaurantCard></RestaurantCard>
+        <RestaurantCard></RestaurantCard>
+        <RestaurantCard></RestaurantCard>
+        <RestaurantCard></RestaurantCard>
+        <RestaurantCard></RestaurantCard>
+        <RestaurantCard></RestaurantCard>
+        <RestaurantCard></RestaurantCard>
+        <RestaurantCard></RestaurantCard>
+        <RestaurantCard></RestaurantCard>
+        <RestaurantCard></RestaurantCard>
       </div>
     </div>
   );
