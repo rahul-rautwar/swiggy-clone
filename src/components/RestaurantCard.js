5e3ccaba887246fd510fbcb9a18f2addbda3fbf6
rahul-rautwar <rahulrautwar@gmail.com>
2023-07-04 23:03:32 +0530

    update restro card

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -1,17 +1,23 @@
 import React from "react";
 
-const RestaurantCard = () => {
+//destructuring on the fly or props can be used
+const RestaurantCard = (props) => {
+  const { resData } = props;
   return (
     <div className="res-card">
       <img
         className="res-logo"
         alt="restaurant logo"
-        src="https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/e0vvulfbahjxjz6k4uwi"
+        src={
+          "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/" +
+          resData?.data?.cloudinaryImageId
+        }
       />
-      <h3>Meghana Foods</h3>
-      <h4>Biryani, North Indian, Asian</h4>
-      <h4>4.4 stars</h4>
-      <h4>35 minutes</h4>
+      <h3>{resData?.data?.name}</h3>
+      <h4>{resData?.data?.cuisines.join(", ")}</h4>
+      <h4>{resData?.data?.avgRating} stars</h4>
+      <h4>₹{resData?.data?.costForTwo / 100} FOR TWO</h4>
+      <h4>{resData?.data?.deliveryTime} minutes </h4>
     </div>
   );
 };
