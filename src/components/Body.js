91c930591e16ab88fbe6f4d5a25ce9852d3f8a70
rahul-rautwar <rahulrautwar@gmail.com>
2023-07-06 23:21:36 +0530

    add state for listOfRestaurants

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import RestaurantCard from "./RestaurantCard";
 import { resData } from "../utils/mockData";
+// import  from "react";
 
 const Body = () => {
-  let listOfRestaurants = [
+  const [listOfRestaurants, setListOfRestaurants] = useState([
     {
       data: {
         id: "29063",
@@ -42,15 +43,34 @@ const Body = () => {
         avgRating: "4.4",
       },
     },
-  ];
+    {
+      data: {
+        id: "29066",
+        name: "KFC 2",
+        cloudinaryImageId: "lx0tpfgsbqygafwh30wh",
+        cuisines: [
+          "Asian",
+          "Chinese",
+          "Vietnamese",
+          "Malaysian",
+          "Sushi",
+          "Singaporean",
+          "Desserts",
+        ],
+        costForTwo: 130000,
+        deliveryTime: 19,
+        avgRating: "4.4",
+      },
+    },
+  ]);
   return (
     <div className="body">
       <div className="filter">
         <button
           className="filter-btn"
           onClick={() => {
-            listOfRestaurants = listOfRestaurants.filter(
-              (res) => res.data.avgRating > 4
+            setListOfRestaurants(
+              listOfRestaurants.filter((res) => res.data.avgRating > 4)
             );
             console.log(listOfRestaurants);
           }}
