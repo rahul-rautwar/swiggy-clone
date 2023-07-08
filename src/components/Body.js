4e86c23fed4eb72170b5709cc86cef5713adbddf
rahul-rautwar <rahulrautwar@gmail.com>
2023-07-08 11:03:06 +0530

    fetch data using useEffect hook

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,10 +1,24 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import RestaurantCard from "./RestaurantCard";
 import { resData } from "../utils/mockData";
 // import  from "react";
 
 const Body = () => {
-  const [listOfRestaurants, setListOfRestaurants] = useState(resData);
+  const [listOfRestaurants, setListOfRestaurants] = useState([]);
+  // [] - will be callend after render
+  // anyVarName - then called after anyVarName changed
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json = await data.json();
+    setListOfRestaurants(json?.data?.cards[2]?.data?.data?.cards);
+  };
+
   return (
     <div className="body">
       <div className="filter">
