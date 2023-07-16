7cb77db4b3dbce2580860963341d77a2c43c9a4e
rahul-rautwar <rahulrautwar@gmail.com>
2023-07-16 16:27:33 +0530

    add searchbox

--- src/components/Body.js
+++ src/components/Body.js
@@ -6,6 +6,7 @@ import { RESTAUARNTS_URL } from "../utils/constant";
 
 const Body = () => {
   const [listOfRestaurants, setListOfRestaurants] = useState([]);
+  const [searchText, setSearchText] = useState("");
   // [] - will be callend after render
   // anyVarName - then called after anyVarName changed
   useEffect(() => {
@@ -24,6 +25,28 @@ const Body = () => {
   ) : (
     <div className="body">
       <div className="filter">
+        <div className="search">
+          <input
+            type="text"
+            className="search-btn"
+            value={searchText}
+            onChange={(event) => {
+              setSearchText(event.target.value);
+            }}
+          />
+          <button
+            type="button"
+            className=""
+            onClick={() => {
+              const filteredRestuarants = listOfRestaurants.filter((res) =>
+                res.data.name.toLowerCase().includes(searchText.toLowerCase())
+              );
+              setListOfRestaurants(filteredRestuarants);
+            }}
+          >
+            Search
+          </button>
+        </div>
         <button
           className="filter-btn"
           onClick={() => {
