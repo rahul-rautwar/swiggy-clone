1a201d1a6b42e40c5c875318e50935d22401240f
rahul-rautwar <rahulrautwar@gmail.com>
2023-07-19 16:09:20 +0530

    add routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import "./App.css";
 import Header from "./components/Header";
 import Body from "./components/Body";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import About from "./components/About";
+
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <Body />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+]);
 
 function App() {
   return (
     <div className="app">
       <Header />
-      <Body />
+      <RouterProvider router={appRouter} />
     </div>
   );
 }
