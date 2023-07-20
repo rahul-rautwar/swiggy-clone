d822890e8039155b07a3645c5d3d740892c5c0a1
rahul-rautwar <rahulrautwar@gmail.com>
2023-07-20 20:48:20 +0530

    update routing with children

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
 import "./App.css";
 import Header from "./components/Header";
 import Body from "./components/Body";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
 import About from "./components/About";
 import { Contact } from "./components/Contact";
 
+const RouteComponent = () => {
+  return <Outlet />;
+};
 const appRouter = createBrowserRouter([
   {
     path: "/",
-    element: <Body />,
-  },
-  {
-    path: "/about",
-    element: <About />,
-  },
-  {
-    path: "/contact",
-    element: <Contact />,
+    element: <RouteComponent />,
+    children: [
+      {
+        path: "/",
+        element: <Body />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+    ],
   },
 ]);
 
