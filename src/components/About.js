f7cead0bb3d131d6188143938f57f3de1bd50b7e
rahul-rautwar <rahulrautwar@gmail.com>
2023-08-01 19:55:06 +0530

    add user component

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,13 @@
 import React from "react";
+import User from "./User";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div>
+      About
+      <User name="Rahul Rautwar"></User>
+    </div>
+  );
 };
 
 export default About;
---
+++ src/components/User.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const User = ({ name }) => {
+  return (
+    <div>
+      <h2>Name: {name}</h2>
+      <h3>location: India</h3>
+      <h4>Contact: @rahulrautwar</h4>
+    </div>
+  );
+};
+
+export default User;
