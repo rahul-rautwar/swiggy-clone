cfc6b224cc266fe0ef292becb5b3b58e3954cf8f
rahul-rautwar <rahulrautwar@gmail.com>
2023-08-05 22:22:53 +0530

    object destructuring

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -4,18 +4,26 @@ import { CDN_URL } from "../utils/constant";
 //destructuring on the fly or props can be used
 const RestaurantCard = (props) => {
   const { resData } = props;
+  const {
+    cloudinaryImageId,
+    name,
+    cuisines,
+    avgRating,
+    costForTwo,
+    deliveryTime,
+  } = resData;
   return (
     <div className="res-card">
       <img
         className="res-logo"
         alt="restaurant logo"
-        src={CDN_URL + resData?.cloudinaryImageId}
+        src={CDN_URL + cloudinaryImageId}
       />
-      <h3>{resData?.name}</h3>
-      <h4>{resData?.cuisines.join(", ")}</h4>
-      <h4>{resData?.avgRating} stars</h4>
-      <h4>₹{resData?.costForTwo / 100} FOR TWO</h4>
-      <h4>{resData?.deliveryTime} minutes </h4>
+      <h3>{name}</h3>
+      <h4>{cuisines.join(", ")}</h4>
+      <h4>{avgRating} stars</h4>
+      <h4>₹{costForTwo / 100} FOR TWO</h4>
+      <h4>{deliveryTime} minutes </h4>
     </div>
   );
 };
