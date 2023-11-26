ffae66440f25d48c9440b4990ac60d4b7ba0856c
rahul-rautwar <rahulrautwar@gmail.com>
2023-11-26 22:15:20 +0530

    update restaurant card

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -13,7 +13,10 @@ const RestaurantCard = (props) => {
     deliveryTime,
   } = resData;
   return (
-    <div className="m-4 p-4 w-[250px] bg-gray-50  rounded-lg hover:bg-gray-200">
+    <div
+      data-testid="restCard"
+      className="m-4 p-4 w-[250px] bg-gray-50  rounded-lg hover:bg-gray-200"
+    >
       <img
         className="rounded-lg"
         alt="restaurant logo"
