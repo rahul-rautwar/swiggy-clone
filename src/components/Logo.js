6f617aed612158f6d18202a925a8a226ea05698d
rahul-rautwar <rahulrautwar@gmail.com>
2024-01-14 20:14:04 +0530

    add link on logo

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { LOGO_URL } from "../utils/constant";
-
+import { Link } from "react-router-dom";
 const Logo = () => {
   return (
     <div className="logo-wrap">
-      <img alt="logo" className="w-[15rem]" src={LOGO_URL} />
+      <Link to="/">
+        <img alt="logo" className="w-[15rem]" src={LOGO_URL} />
+      </Link>
     </div>
   );
 };
